e189eacf4f3bb552dc081feab24afee4ae2d0a4e
kaizi2009 <kaizi_2009@126.com>
2017-09-04 12:54:04 +0800

    init

---
+++ src/main/java/com/zhangkai/javautil/HashMapTest.java
@@ -0,0 +1,13 @@
+package com.zhangkai.javautil;
+
+import java.util.HashMap;
+
+/**
+ * Created by zhangkai on 2017/9/4.
+ */
+public class HashMapTest {
+    public static void main(String[] args) {
+        HashMap map = new HashMap();
+        map.put("",1);
+    }
+}
