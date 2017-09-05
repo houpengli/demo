2e8ab0f63168c6a1f21c96163fae0612227e5cac
kaizi2009 <kaizi_2009@126.com>
2017-09-05 13:36:09 +0800

    init

---
+++ src/main/java/com/zhangkai/lock/queue/QueueTest.java
@@ -0,0 +1,10 @@
+package com.zhangkai.lock.queue;
+
+/**
+ * 1.库存拆分与合并
+ * 2.分布式锁
+ * 3.实施的阻塞并发处理(不使用异步处理)
+ * Created by zhangkai on 2017/9/5.
+ */
+public class QueueTest {
+}
---
+++ src/main/java/com/zhangkai/lock/random/RandomTest.java
@@ -0,0 +1,12 @@
+package com.zhangkai.lock.random;
+
+import java.util.Random;
+
+/**
+ * Created by zhangkai on 2017/9/5.
+ */
+public class RandomTest {
+    public static void main(String[] args) {
+        System.out.println(new Random().nextInt(10000));
+    }
+}
