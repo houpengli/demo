49a44f4ab126c86e936c8f31a2de759700f8a509
kaizi2009 <kaizi_2009@126.com>
2017-09-14 18:58:15 +0800

    init

---
+++ src/main/java/com/zhangkai/concurrent/TestConcurrentHashMap.java
@@ -0,0 +1,14 @@
+package com.zhangkai.concurrent;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Created by zhangkai on 2017/9/14.
+ */
+public class TestConcurrentHashMap {
+    public static void main(String[] args) {
+        ConcurrentHashMap map = new ConcurrentHashMap();
+        map.put("","");
+
+    }
+}
